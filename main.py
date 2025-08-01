27a67112b8faf41ce5fd4baab5acfad8280104c5
anyoak <anyoakmail@gmail.com>
2025-08-02 01:11:48 +0600

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ usdt_abi = [
     {
         "constant": True,
         "inputs": [{"name": "_owner", "type": "address"}],
-        "name极速赛车("balanceOf",
+        "name": "balanceOf",
         "outputs": [{"name": "balance", "type": "uint256"}],
         "type": "function"
     }
